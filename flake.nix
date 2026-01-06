9653eb1293adef52e41c49a3452963094fcbfd0c
Wayne Van Son <waynevanson@gmail.com>
2026-01-07 07:33:35 +1100

    build: init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit system;
+        packages = with pkgs; [
+          nodejs_24
+          corepack_24
+        ];
+      };
+    });
+}
