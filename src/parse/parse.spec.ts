9ae09f16826581a5b1142ddc7010b1843cea6a72
Wayne Van Son <waynevanson@gmail.com>
2026-01-14 09:00:00 +1100

    feat: add multi spaced flag

--- src/parse/parse.spec.ts
+++ src/parse/parse.spec.ts
@@ -123,4 +123,17 @@ describe(parse, () => {
     const output = v.parse(schema, input)
     expect(output).toStrictEqual("hello")
   })
+
+  test("long flags option into value", () => {
+    const schema = option(v.array(v.string()), {
+      type: "option",
+      name: "greeting",
+      longs: ["greeting"],
+      shorts: []
+    })
+    const args = ["--greeting", "hello", "--greeting", "world"]
+    const input = parse(schema, args)
+    const output = v.parse(schema, input)
+    expect(output).toStrictEqual(["hello", "world"])
+  })
 })
