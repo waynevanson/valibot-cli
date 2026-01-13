f1576a720d160a6d83c5642f79095bdef1550f45
Wayne Van Son <waynevanson@gmail.com>
2026-01-13 21:29:23 +1100

    feat: use new parser technique

--- src/parse/parse.ts
+++ src/parse/parse.ts
@@ -3,51 +3,97 @@ import {
   ArgMethod,
   ArgOptionMetadata,
   ArgValueMetadata,
-  isArgMethod
+  getArgMethodMetadata,
+  isArgOptionMetadata
 } from "../methods"
-import {
-  ArgsToken,
-  createArgsTokens,
-  OptionLongToken,
-  OptionShortToken
-} from "./arg-token"
+import { createArgsTokens } from "./arg-token"
+
+// at the start we allow all args
+// but we eventually it gets narrowed down
+export type Args = Array<{
+  name: string
+  value: string
+}>
+
+export interface Matches {
+  args: Args
+  subcommand:
+    | undefined
+    | {
+        name: string
+        matches: Matches
+      }
+}
 
 export type ParsableSchema = ArgMethod<
   v.StringSchema<v.ErrorMessage<v.StringIssue> | undefined>,
   ArgValueMetadata | ArgOptionMetadata
 >
 
-// Build the input value based on the schema and CLI args
 export function parse<TSchema extends ParsableSchema>(
   schema: TSchema,
   args: Array<string>
 ): v.InferInput<TSchema> {
   const tokens = createArgsTokens(args)
-  let flageRequiresValue: undefined | OptionLongToken | OptionShortToken
 
-  // we probably need a pointer to an object I create
-  const ref: { value: unknown } = {
-    value: undefined
-  }
+  const matches: Matches = { args: [], subcommand: undefined }
 
   for (const token of tokens) {
     switch (token.type) {
       case "option": {
-        // find name via long or short name
-        if (token.short) {
-        } else {
+        if (token.short || token.value === undefined) {
+          throw new Error()
         }
 
-        //
-        if (token.value) {
-        } else {
+        const metadata = getArgMethodMetadata(schema)
+
+        if (!isArgOptionMetadata(metadata)) {
+          throw new Error()
         }
+
+        matches.args.push({ name: metadata.name, value: token.value })
 
         break
       }
-      case "value": {
-        break
+      default:
+        throw new Error()
+    }
+  }
+
+  // build data here
+
+  switch (schema.type) {
+    case "string": {
+      const metadata = getArgMethodMetadata(schema)
+
+      if (!isArgOptionMetadata(metadata)) {
+        throw new Error()
       }
+
+      let multiple = false
+      let value: undefined | string = undefined
+      for (const match of matches.args) {
+        if (match.name === metadata.name) {
+          if (value !== undefined) {
+            multiple = true
+          }
+          value = match.value
+        }
+      }
+
+      if (value === undefined) {
+        throw new Error()
+      }
+
+      if (multiple) {
+        throw new Error()
+      }
+
+      return value
+      break
+    }
+    default: {
+      throw new Error()
     }
   }
 }
