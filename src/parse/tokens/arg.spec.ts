829b5f4c1fe46782733d4ccb08a3ec00893cd6cf
Wayne Van Son <waynevanson@gmail.com>
2026-01-25 18:56:31 +1100

    test: add arg.spec.ts for prop testing

--- src/parse/tokens/arg.spec.ts
+++ src/parse/tokens/arg.spec.ts
@@ -1,13 +1,17 @@
 import { fc, test } from "@fast-check/vitest";
 import { describe, expect } from "vitest";
-import { createArgTokens } from "./arg.js";
+import {
+  createArgTokens,
+  type OptionShortToken,
+  type OptionsShortValueToken,
+} from "./arg.js";
 
 describe(createArgTokens, () => {
   const chars = fc
     .string({ minLength: 1 })
     .filter((string) => /^[^\s-=]+$/.test(string));
 
-  test.prop([chars])("", (chars) => {
+  test.prop([chars])("OptionShortToken", (chars) => {
     const arg = `-${chars}`;
     const argTokens = createArgTokens(arg);
 
@@ -20,4 +24,41 @@ describe(createArgTokens, () => {
 
     expect(argTokens).toStrictEqual(expected);
   });
+
+  const value = fc
+    .string({ minLength: 1 })
+    .filter((string) => !string.includes(" "));
+
+  test.prop([chars, value], { verbose: true })(
+    "OptionShortValueToken",
+    (chars, value) => {
+      const arg = `-${chars}=${value}`;
+      const argTokens = createArgTokens(arg);
+
+      const bodies = chars
+        .slice(0, -1)
+        .split("")
+        .map(
+          (char): OptionShortToken => ({
+            identifier: char,
+            short: true,
+            type: "option",
+            value: undefined,
+          }),
+        );
+
+      const optionShortTokens = argTokens.slice(0, -1);
+      expect(optionShortTokens).toStrictEqual(bodies);
+
+      const last: OptionsShortValueToken = {
+        type: "option",
+        identifier: chars[chars.length - 1],
+        short: true,
+        value,
+      };
+
+      const optionShortValueToken = argTokens.at(-1);
+      expect(optionShortValueToken).toStrictEqual(last);
+    },
+  );
 });
