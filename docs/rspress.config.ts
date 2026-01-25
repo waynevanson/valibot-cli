3c0214664f3082552e667089bd6dd6b9c3227bcd
Wayne Van Son <waynevanson@gmail.com>
2026-01-25 17:44:13 +1100

    format

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,20 +1,20 @@
-import * as path from "node:path";
-import { defineConfig } from "@rspress/core";
+import * as path from 'node:path';
+import { defineConfig } from '@rspress/core';
 
 export default defineConfig({
-  root: path.join(__dirname, "src"),
-  title: "My Site",
-  icon: "/rspress-icon.png",
+  root: path.join(__dirname, 'src'),
+  title: 'My Site',
+  icon: '/rspress-icon.png',
   logo: {
-    light: "/rspress-light-logo.png",
-    dark: "/rspress-dark-logo.png",
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
   },
   themeConfig: {
     socialLinks: [
       {
-        icon: "github",
-        mode: "link",
-        content: "https://github.com/waynevanson/valibot-cli",
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/waynevanson/valibot-cli',
       },
     ],
   },
