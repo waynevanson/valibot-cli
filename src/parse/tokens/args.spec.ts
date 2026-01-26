1de4d56edfa505403385cee6f42fb086263df9cb
Wayne Van Son <waynevanson@gmail.com>
2026-01-26 20:51:39 +1100

    test: add args.spec

---
+++ src/parse/tokens/args.spec.ts
@@ -0,0 +1,92 @@
+import { fc, test } from "@fast-check/vitest";
+import { describe, expect } from "vitest";
+import type {
+  OptionLongToken,
+  OptionShortToken,
+  OptionsShortValueToken,
+  ValueToken,
+} from "./arg.js";
+import { createArgsTokens } from "./args.js";
+
+describe(createArgsTokens, () => {
+  const chars = fc
+    .string({ minLength: 1 })
+    .filter((string) => /^[^\s-=]+$/.test(string));
+
+  test.prop([chars])("OptionShortToken", (chars) => {
+    const arg = `-${chars}`;
+    const argTokens = Array.from(createArgsTokens([arg]));
+
+    const expected = chars.split("").map((char) => ({
+      identifier: char,
+      short: true,
+      type: "option",
+      value: undefined,
+    }));
+
+    expect(argTokens).toStrictEqual(expected);
+  });
+
+  const optionValue = fc
+    .string({ minLength: 1 })
+    .filter((string) => !string.includes(" "));
+
+  test.prop([chars, optionValue], { verbose: true })(
+    "OptionShortValueToken",
+    (chars, value) => {
+      const arg = `-${chars}=${value}`;
+      const argTokens = Array.from(createArgsTokens([arg]));
+
+      const bodies = chars
+        .slice(0, -1)
+        .split("")
+        .map(
+          (char): OptionShortToken => ({
+            identifier: char,
+            short: true,
+            type: "option",
+            value: undefined,
+          }),
+        );
+
+      const optionShortTokens = argTokens.slice(0, -1);
+      expect(optionShortTokens).toStrictEqual(bodies);
+
+      const last: OptionsShortValueToken = {
+        type: "option",
+        identifier: chars[chars.length - 1],
+        short: true,
+        value,
+      };
+
+      const optionShortValueToken = argTokens.at(-1);
+      expect(optionShortValueToken).toStrictEqual(last);
+    },
+  );
+
+  const value = fc
+    .string({ minLength: 1 })
+    .filter((string) => /^[^(--)][^\s=]+$/.test(string));
+
+  test.prop([value])("ValueToken", (value) => {
+    const argTokens = Array.from(createArgsTokens([value]));
+    const expected: ValueToken = { type: "value", value };
+    expect(argTokens).toStrictEqual([expected]);
+  });
+
+  const longIdentifier = fc
+    .string({ minLength: 1 })
+    .filter((string) => /^(?!<(--))[^\s=]+$/.test(string));
+
+  test.prop([longIdentifier])("OptionLongToken", (identifier) => {
+    const argTokens = Array.from(createArgsTokens([`--${identifier}`]));
+    expect(argTokens).toStrictEqual([
+      {
+        type: "option",
+        short: false,
+        identifier,
+        value: undefined,
+      } satisfies OptionLongToken,
+    ]);
+  });
+});
